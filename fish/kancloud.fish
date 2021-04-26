6f7c95fe27351148113a3bf551e5f21a11f56ee9
yuan_hp <yuan_hp@qq.com>
2021-04-26 10:56:45 +0800

    2021/04/26 10:56:45

--- fish/kancloud.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-#UFUNCTION=打开看云
-function kancloud 
-    if test -z $argv[1]
-        open https://www.kancloud.cn/dashboard
-    else if test $argv[1] = "-help"
-        echo "  该指令将会在默认浏览器中打开gitee。"
-    end
-end
