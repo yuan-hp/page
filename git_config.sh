e3abac2b388b6acd77066d8473de15aff1f0133f
yhp <yuan_hp@qq.com>
2023-11-25 15:36:11 +0800

    2023/11/25 15:36:11

---
+++ git_config.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------
+#	FileName	: git_config.sh
+#	Author		：hpy
+#	Date		：2023年11月25日
+#	Description	：
+#-------------------------------------------------------
+
+email="yuan_hp@qq.com"
+
+ssh-keygen -t rsa -C "$email"
+
+
+git config --global user.name "$(whoami)"
+git config --global user.email "$email"
+
